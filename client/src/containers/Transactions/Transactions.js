8be09da5c257e01ada0b03524a2f571c2fafd467
ArielFeingold <feingold.ariel@gmail.com>
2018-10-10 16:41:14 -0400

    transactions page template

--- client/src/containers/Transactions/Transactions.js
+++ client/src/containers/Transactions/Transactions.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem } from 'reactstrap';
 
 class Transactions extends Component {
 
   render(){
     return(
-      <>
+      <Container className="mt-5 ml-5 mr-5">
         <h1>Transactions</h1>
-      </>
+        <ListGroup>
+          <ListGroupItem>Cras justo odio</ListGroupItem>
+          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+          <ListGroupItem>Morbi leo risus</ListGroupItem>
+          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+        </ListGroup>
+      </Container>
     )
   }
 }
