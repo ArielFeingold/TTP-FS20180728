b1ed69b50edd942caaeab5c4b29c513b20385fa6
ArielFeingold <feingold.ariel@gmail.com>
2018-10-10 16:43:02 -0400

    Transactions connected to Redux store

--- client/src/containers/Transactions/Transactions.js
+++ client/src/containers/Transactions/Transactions.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
 import { Container, ListGroup, ListGroupItem } from 'reactstrap';
 
 class Transactions extends Component {
@@ -19,4 +21,16 @@ class Transactions extends Component {
   }
 }
 
-export default Transactions;
+const mapStateToProps = state => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect( mapStateToProps, mapDispatchToProps ) (Transactions)
